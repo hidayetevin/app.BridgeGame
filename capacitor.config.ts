2259f9da739b260169230f4a45b4f1ccdcde286c
hidayetevin <92186456+hidayetevin@users.noreply.github.com>
2026-02-25 00:21:13 +0300

    reklam görüntüsü için ayarlar

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.bridge.game',
   appName: 'BridgeGame',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    StatusBar: {
+      overlaysWebView: false
+    }
+  }
 };
 
 export default config;
