a2746e89a3591ac9dac2a5a50a13c75c193ef591
hidayetevin <92186456+hidayetevin@users.noreply.github.com>
2026-02-26 00:46:21 +0300

    yayın ayarları oyun adı kimliği

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.bridge.game',
-  appName: 'BridgeGame',
+  appId: 'com.evnlabs.bridgemaster',
+  appName: 'Bridge Master',
   webDir: 'dist',
   plugins: {
     StatusBar: {
